358e01b93d5981acc80ebe186d3a6ebfe886d172
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-05-17 19:24:26 +0500

    Comperition

---
+++ practice.7/practice.js
@@ -0,0 +1,26 @@
+// Comperition //
+// ==========<,>,==,===,>==,<==
+// taqoslash true, folse,kaytaradi
+// < katta, >kichkina, = kiymat biriktirish
+// == taqoslash, === data  taypini ham tekshiradi
+// let a = 10;
+// let b = 11;
+// let  c = 11;
+// let  v ="11";
+//console.log(a < b);//  a10 < b11 true
+//console.log(a > b);//  a10 > b11 folse
+//console.log(c > b) //  c11 > b11 folse
+//console.log(c >= b)//   c11 >= b11 true
+//console.log(b <= b);//  b11 <= b11 true
+//console.log(c == b);//  c11 == b11 true
+//console.log(c === v);//  c11 === v11 <string folse
+
+//============ hariflarda taqoslash a A
+// let str1 ="A"
+// let str2 ="a"
+// console.log("A" > "a") //false A93 > a98
+// console.log("A" < "a") //true  A93 < a98
+// console.log("A" == "a") //false A93 == a98
+// console.log("A" === "a") //false A93 === a98
+
+// ====================
