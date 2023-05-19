0a52faed708756b1eb7e84dc224b72b6b3715bb1
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-05-19 19:20:27 +0500

    null vs undefined

---
+++ practice.6/practice.js
@@ -0,0 +1,13 @@
+// null === undefined // =======[
+// console.log(null == undefined);//true shikalat bormi yuqmi yuq
+// console.log(null === undefined);//false shikalati qog'ozichi bor
+// console.log(false == undefined);//false
+// console.log(false === undefined);//false
+// console.log( false == true);//false
+// console.log(false === true);//false
+// console.log( false == null);//false
+// console.log(false === null);//false
+// console.log( false == false);//true
+// console.log(null == null);//true
+// console.log(null === null);//true
+//=====================]
