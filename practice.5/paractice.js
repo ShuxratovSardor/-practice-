23be57a26901185f90784c6216e6587fd026052a
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-05-20 18:40:03 +0500

    Nullish

---
+++ practice.5/paractice.js
@@ -0,0 +1,15 @@
+//===== Nullish ?? false ga ishledi -> , null,undefined keganda keyingisiga utkazib yuboradi ====//
+//==== OR || ,null , undefined, 0, false, NaN.===//
+// console.log(null ?? "web");//web
+// console.log(undefined ?? 1);//1
+// console.log(false || 1);// 1
+// console.log(0 ?? 1 );// 0
+// console.log(12 ?? null);//12
+// console.log(NaN ?? 1);//NaN
+// console.log(false ?? true);//false
+// console.log( false ?? 0);//false
+// console.log(NaN || 1);//1
+// console.log(null || 1);//1
+// console.log(null ?? 1);//1
+// console.log(undefined || 1);//1
+// console.log(undefined ?? 1);//1
